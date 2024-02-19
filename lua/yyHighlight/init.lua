4868e0d15e0e9db56eb386bdaa9e1a24ddc9c541
D4ryB3rry <d4ryb3rry@gmail.com>
2024-02-19 22:39:57 +0100

    Optimize highlighting.lua

--- lua/yyHighlight/init.lua
+++ lua/yyHighlight/init.lua
@@ -13,36 +13,34 @@ function M.setup(user_config)
 end
 
 M.highlight = function()
-    local startPos = vim.api.nvim_buf_get_mark(0, "[")
-    local endPos = vim.api.nvim_buf_get_mark(0, "]")
-
-    -- Use the configured highlight group and color
-    local highlightGroup = M.config.highlight_group
-    vim.cmd("highlight " .. highlightGroup .. " guibg=" .. M.config.highlight_color)
-
-    local ns_id = vim.api.nvim_create_namespace('yyHighlight')
     local buf = vim.api.nvim_get_current_buf()
+    local ns_id = vim.api.nvim_create_namespace('yyHighlight')
 
-    -- Adjust startCol and endCol to use the actual column positions
+    -- Consolidate retrieval of start and end positions to reduce calls
+    local startPos, endPos = vim.api.nvim_buf_get_mark(buf, "["), vim.api.nvim_buf_get_mark(buf, "]")
+
+    -- Directly use 'M.config' table to access configurations
+    -- Combine highlight setup into one command if possible and check if highlight group needs to be redefined
+    local highlightGroup = M.config.highlight_group
+    local highlightCmd = string.format("highlight %s guibg=%s", highlightGroup, M.config.highlight_color)
+    vim.cmd(highlightCmd)
+
+    -- Adjust line and column numbers for zero-based indexing and inclusive highlighting
     local startLine, startCol = startPos[1] - 1, startPos[2]
-    local endLine, endCol = endPos[1] - 1, endPos[2]
+    local endLine, endCol = endPos[1] - 1, endPos[2] + 1 -- Make endCol inclusive in the highlight
 
-    -- Fetch the text of the end line to ensure end_col is within range and adjust for inclusive highlighting
+    -- Adjust endCol based on the length of the end line to ensure it doesn't exceed the line length
     local endLineText = vim.api.nvim_buf_get_lines(buf, endLine, endLine + 1, false)[1]
-    -- Increase endCol by 1 to make highlighting inclusive of the last character
-    endCol = endCol + 1
-    -- Ensure endCol does not exceed the length of the line
-    if endCol > #endLineText then
-        endCol = #endLineText
-    end
+    endCol = math.min(endCol, #endLineText) -- Ensure endCol does not exceed line length
 
+    -- Set the highlight and use table to pass parameters for clarity
     vim.api.nvim_buf_set_extmark(buf, ns_id, startLine, startCol, {
         end_line = endLine,
         end_col = endCol,
-        hl_group = highlightGroup
+        hl_group = highlightGroup,
     })
 
-    -- Use the configured highlight duration
+    -- Clear the highlight after a delay, using 'M.config.highlight_duration'
     vim.defer_fn(function()
         vim.api.nvim_buf_clear_namespace(buf, ns_id, 0, -1)
     end, M.config.highlight_duration)
