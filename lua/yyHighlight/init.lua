0e8ec35731e85dfa6adbefca20f0322f25b1b770
D4ryB3rry <D4ryB3rry@gmail.com>
2024-02-19 16:30:09 +0100

    init lua changed

--- lua/yyHighlight/init.lua
+++ lua/yyHighlight/init.lua
@@ -12,7 +12,6 @@ function M.setup(user_config)
     M.config = vim.tbl_extend("force", M.config, user_config)
 end
 
-
 M.highlight = function()
     local startPos = vim.api.nvim_buf_get_mark(0, "[")
     local endPos = vim.api.nvim_buf_get_mark(0, "]")
@@ -24,10 +23,15 @@ M.highlight = function()
     local ns_id = vim.api.nvim_create_namespace('yyHighlight')
     local buf = vim.api.nvim_get_current_buf()
 
-    local startLine, startCol = startPos[1] - 1, 0
-    local endLine = endPos[1] - 1
-    local endLineText = vim.api.nvim_buf_get_lines(buf, endLine, endLine + 1, false)[1] or ""
-    local endCol = #endLineText
+    -- Adjust startCol and endCol to use the actual column positions
+    local startLine, startCol = startPos[1] - 1, startPos[2]
+    local endLine, endCol = endPos[1] - 1, endPos[2]
+
+    -- Correct endCol for zero-index and to include the last character
+    -- Lua indexes from 1, but Vim's API indexes from 0 for columns
+    if endCol > 0 then
+        endCol = endCol - 1
+    end
 
     vim.api.nvim_buf_set_extmark(buf, ns_id, startLine, startCol, {
         end_line = endLine,
